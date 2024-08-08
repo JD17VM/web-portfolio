f7d0e7f523da7cf320b295445bdedf0d20bc44e7
JD17VM <jdiegodiego4@gmail.com>
2024-08-08 17:36:02 -0500

    :lipstick: feat: add base styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import './assets/styles/normalize.css'
+import './assets/styles/estilos_generales.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
