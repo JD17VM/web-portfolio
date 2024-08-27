8b0fb2954c4d8830359fe49190df110a810dedd9
JD17VM <jdiegodiego4@gmail.com>
2024-08-26 23:52:37 -0500

    :sparkles: feat: add handle scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,86 @@
-import styles from './assets/styles/estilos_pagina_inicio.module.scss'
-import { FaGithub } from "react-icons/fa";
+import React, { useEffect, useState, useRef } from 'react';
+import styles from './assets/styles/estilos_pagina_inicio.module.scss';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import { FaLinkedin } from "react-icons/fa";
-
-import { JobCard, ProjectCard } from './widgets/JobCard'
+import { JobCard, ProjectCard } from './widgets/JobCard';
 
 function App() {
+  const [activeSection, setActiveSection] = useState('section1');
+
+  // Referencias a las secciones
+  const section1Ref = useRef(null);
+  const section2Ref = useRef(null);
+  const section3Ref = useRef(null);
+  const section4Ref = useRef(null);
+
+  // Función throttle para mejorar el rendimiento
+  const throttle = (func, limit) => {
+    let inThrottle;
+    return function (...args) {
+      if (!inThrottle) {
+        func.apply(this, args);
+        inThrottle = true;
+        setTimeout(() => (inThrottle = false), limit);
+      }
+    };
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+
+      // Verificar en qué sección está el usuario
+      if (scrollPosition < section2Ref.current.offsetTop - 100) {
+        setActiveSection('section1');
+      } else if (scrollPosition < section3Ref.current.offsetTop - 100) {
+        setActiveSection('section2');
+      } else if (scrollPosition < section4Ref.current.offsetTop - 100) {
+        setActiveSection('section3');
+      } else {
+        setActiveSection('section4');
+      }
+    };
+
+    // Añadir throttle al evento scroll
+    const throttledScroll = throttle(handleScroll, 100);
+    window.addEventListener('scroll', throttledScroll);
+
+    // Limpiar el event listener al desmontar el componente
+    return () => {
+      window.removeEventListener('scroll', throttledScroll);
+    };
+  }, []);
+
+  const handleNavClick = (e, sectionId) => {
+    e.preventDefault();
+    let section;
+
+    // Obtener la referencia correcta según el sectionId
+    switch (sectionId) {
+      case 'section1':
+        section = section1Ref.current;
+        break;
+      case 'section2':
+        section = section2Ref.current;
+        break;
+      case 'section3':
+        section = section3Ref.current;
+        break;
+      case 'section4':
+        section = section4Ref.current;
+        break;
+      default:
+        section = section1Ref.current;
+    }
+
+    // Desplazamiento suave
+    window.scrollTo({
+      top: section.offsetTop - 50,
+      behavior: 'smooth'
+    });
+
+    setActiveSection(sectionId);
+  };
 
   return (
     <div className={styles['container-total']}>
@@ -17,9 +92,42 @@ function App() {
 
           <nav>
             <ul>
-              <li>ABOUT</li>
-              <li>EXPERIENCE</li>
-              <li>PROJECTS</li>
+              <li>
+                <a
+                  href="#section1"
+                  onClick={(e) => handleNavClick(e, 'section1')}
+                  className={activeSection === 'section1' ? styles.active : ''}
+                >
+                  ABOUT ME
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#section2"
+                  onClick={(e) => handleNavClick(e, 'section2')}
+                  className={activeSection === 'section2' ? styles.active : ''}
+                >
+                  INTERESTS
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#section3"
+                  onClick={(e) => handleNavClick(e, 'section3')}
+                  className={activeSection === 'section3' ? styles.active : ''}
+                >
+                  EXPERIENCE
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#section4"
+                  onClick={(e) => handleNavClick(e, 'section4')}
+                  className={activeSection === 'section4' ? styles.active : ''}
+                >
+                  PROJECTS
+                </a>
+              </li>
             </ul>
           </nav>
 
@@ -30,29 +138,29 @@ function App() {
           </div>
 
           <div className={styles['container-languages']}>
-            <h2>LANGUAJES</h2>
+            <h2>LANGUAGES</h2>
             <ul>
               <li><p>Spanish:</p> <p>Native</p></li>
               <li><p>English:</p> <p>Proficient</p></li>
-              <li><p>Portugues:</p> <p>Intermediate</p></li>
+              <li><p>Portuguese:</p> <p>Intermediate</p></li>
             </ul>
           </div>
         </div>
       </div>
-      
+
       <div className={styles['container-text-title']}>
-        <div className={styles['text-title']}>
+        <div className={styles['text-title']} ref={section1Ref} id="section1">
           <h2>ABOUT</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid beatae hic excepturi eos illo ex ut vero ipsam ad? Obcaecati et doloribus, incidunt distinctio facilis atque velit. In eligendi corporis asperiores rerum aperiam? Exercitationem ipsa similique dolores accusamus. Nemo dolor, quisquam vero laboriosam ex in cupiditate et amet eligendi autem, ipsa hic suscipit, tempore mollitia debitis nisi at! Corporis quam hic fuga, at voluptas cum dolorum assumenda blanditiis consequuntur? Velit molestiae saepe, labore illo minima facere aliquam doloremque. Culpa, itaque?</p>
         </div>
 
-        <div className={styles['text-title']}>
-          <h2>ABOUT</h2>
+        <div className={styles['text-title']} ref={section2Ref} id="section2">
+          <h2>INTERESTS</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid beatae hic excepturi eos illo ex ut vero ipsam ad? Obcaecati et doloribus, incidunt distinctio facilis atque velit. In eligendi corporis asperiores rerum aperiam? Exercitationem ipsa similique dolores accusamus. Nemo dolor, quisquam vero laboriosam ex in cupiditate et amet eligendi autem, ipsa hic suscipit, tempore mollitia debitis nisi at! Corporis quam hic fuga, at voluptas cum dolorum assumenda blanditiis consequuntur? Velit molestiae saepe, labore illo minima facere aliquam doloremque. Culpa, itaque?</p>
         </div>
       </div>
-      
-      <div className={styles['container-experience']}>
+
+      <div className={styles['container-experience']} ref={section3Ref} id="section3">
         <h2>EXPERIENCE</h2>
         <div>
           <JobCard/>
@@ -61,7 +169,7 @@ function App() {
         </div>
       </div>
 
-      <div className={styles['container-projects']}>
+      <div className={styles['container-projects']} ref={section4Ref} id="section4">
         <h2>PROJECTS</h2>
         <div>
           <ProjectCard/>
@@ -72,7 +180,7 @@ function App() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
