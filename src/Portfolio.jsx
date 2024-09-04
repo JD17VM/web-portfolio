5d9484f13697c29a74aa38792d6a8d0ae776b6ed
JD17VM <jdiegodiego4@gmail.com>
2024-09-04 09:21:37 -0500

    :lipstick: style: format jsx code in Portfolio component

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -87,116 +87,116 @@ const Portfolio = () => {
     return (
         <div className='body'>
             <title>Portfolio JD17VM</title>
-        <div className={styles['container-total']}>
-        <div className={styles['container-scroll-info-block']}>
-            <div className={styles['info-block']}>
-            <h1>Juan Diego Valdivia Mendoza</h1>
-            <p>{pageData.job}</p>
-            <p>I build accessible, pixel-perfect digital experiences for the web.</p>
+            <div className={styles['container-total']}>
+                <div className={styles['container-scroll-info-block']}>
+                    <div className={styles['info-block']}>
+                        <h1>Juan Diego Valdivia Mendoza</h1>
+                        <p>{pageData.job}</p>
+                        <p>I build accessible, pixel-perfect digital experiences for the web.</p>
 
-            <nav>
-                <ul>
-                <li>
-                    <a
-                    href="#section1"
-                    onClick={(e) => handleNavClick(e, 'section1')}
-                    className={activeSection === 'section1' ? styles.active : ''}
-                    >
-                    {pageData.title.about}
-                    </a>
-                </li>
-                <li>
-                    <a
-                    href="#section2"
-                    onClick={(e) => handleNavClick(e, 'section2')}
-                    className={activeSection === 'section2' ? styles.active : ''}
-                    >
-                    {pageData.title.interests}
-                    </a>
-                </li>
-                <li>
-                    <a
-                    href="#section3"
-                    onClick={(e) => handleNavClick(e, 'section3')}
-                    className={activeSection === 'section3' ? styles.active : ''}
-                    >
-                    {pageData.title.experience.toUpperCase()}
-                    </a>
-                </li>
-                <li>
-                    <a
-                    href="#section4"
-                    onClick={(e) => handleNavClick(e, 'section4')}
-                    className={activeSection === 'section4' ? styles.active : ''}
-                    >
-                    {pageData.title.projects}
-                    </a>
-                </li>
-                </ul>
-            </nav>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a
+                                        href="#section1"
+                                        onClick={(e) => handleNavClick(e, 'section1')}
+                                        className={activeSection === 'section1' ? styles.active : ''}
+                                    >
+                                        {pageData.title.about}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="#section2"
+                                        onClick={(e) => handleNavClick(e, 'section2')}
+                                        className={activeSection === 'section2' ? styles.active : ''}
+                                    >
+                                        {pageData.title.interests}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="#section3"
+                                        onClick={(e) => handleNavClick(e, 'section3')}
+                                        className={activeSection === 'section3' ? styles.active : ''}
+                                    >
+                                        {pageData.title.experience.toUpperCase()}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="#section4"
+                                        onClick={(e) => handleNavClick(e, 'section4')}
+                                        className={activeSection === 'section4' ? styles.active : ''}
+                                    >
+                                        {pageData.title.projects}
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
 
-            <div className={styles['social-links']}>
-                <a href=""><FaGithub /></a>
-                <a href=""><MdEmail /></a>
-                <a href=""><FaLinkedin /></a>
-            </div>
+                        <div className={styles['social-links']}>
+                            <a href=""><FaGithub /></a>
+                            <a href=""><MdEmail /></a>
+                            <a href=""><FaLinkedin /></a>
+                        </div>
 
-            <div className={styles['container-languages']}>
-                <h2>LANGUAGES</h2>
-                <ul>
-                {pageData.languages.map((language, index) => (
-                    <li key={index}><p>{language.language}:</p> <p>{language.level}</p></li>
-                ))}
-                </ul>
-            </div>
-            </div>
-        </div>
+                        <div className={styles['container-languages']}>
+                            <h2>LANGUAGES</h2>
+                            <ul>
+                                {pageData.languages.map((language, index) => (
+                                    <li key={index}><p>{language.language}:</p> <p>{language.level}</p></li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
 
-        <div className={styles['container-text-title']}>
-            <div className={styles['text-title']} ref={section1Ref} id="section1">
-            <h2>{pageData.title.about}</h2>
-            <p dangerouslySetInnerHTML={{ __html: pageData.about }}></p>
-            </div>
+                <div className={styles['container-text-title']}>
+                    <div className={styles['text-title']} ref={section1Ref} id="section1">
+                        <h2>{pageData.title.about}</h2>
+                        <p dangerouslySetInnerHTML={{ __html: pageData.about }}></p>
+                    </div>
 
-            <div className={styles['text-title']} ref={section2Ref} id="section2">
-            <h2>{pageData.title.interests}</h2>
-            <p>{pageData.interests}</p>
-            </div>
-        </div>
+                    <div className={styles['text-title']} ref={section2Ref} id="section2">
+                        <h2>{pageData.title.interests}</h2>
+                        <p>{pageData.interests}</p>
+                    </div>
+                </div>
 
-        <div className={styles['container-experience']} ref={section3Ref} id="section3">
-            <h2>{pageData.title.experience.toUpperCase()}</h2>
-            <div>
-            {pageData.experience.map((job, index) => (
-                <JobCard 
-                key = {index} 
-                startDate = {job.startYear} 
-                endDate = {job.endYear} 
-                roleCompany = {job.roleCompany} 
-                description = {job.description} 
-                skills ={job.skills} 
-                />
-            ))}
-            </div>
-        </div>
+                <div className={styles['container-experience']} ref={section3Ref} id="section3">
+                    <h2>{pageData.title.experience.toUpperCase()}</h2>
+                    <div>
+                        {pageData.experience.map((job, index) => (
+                            <JobCard
+                                key={index}
+                                startDate={job.startYear}
+                                endDate={job.endYear}
+                                roleCompany={job.roleCompany}
+                                description={job.description}
+                                skills={job.skills}
+                            />
+                        ))}
+                    </div>
+                </div>
 
-        <div className={styles['container-projects']} ref={section4Ref} id="section4">
-            <h2>{pageData.title.projects}</h2>
-            <div>
-            {pageData.projects.map((project, index) => (
-                <ProjectCard
-                key = {index}
-                title = {project.title}
-                img = {project.img}
-                date = {project.date}
-                description = {project.description}
-                skills = {project.skills}
-                link={project.link}
-                />
-            ))}
+                <div className={styles['container-projects']} ref={section4Ref} id="section4">
+                    <h2>{pageData.title.projects}</h2>
+                    <div>
+                        {pageData.projects.map((project, index) => (
+                            <ProjectCard
+                                key={index}
+                                title={project.title}
+                                img={project.img}
+                                date={project.date}
+                                description={project.description}
+                                skills={project.skills}
+                                link={project.link}
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
         </div>
     );
 }
